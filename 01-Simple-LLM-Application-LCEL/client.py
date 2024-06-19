13c9309e7f5acf05b15f357bf379a369c72dcefb
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-06-19 10:25:06 +0530

    Add : Comments in code

--- 01-Simple-LLM-Application-LCEL/client.py
+++ 01-Simple-LLM-Application-LCEL/client.py
@@ -1,4 +1,5 @@
 from langserve import RemoteRunnable
 
+# Creating a remote client to hit the Langchain endpoint with inputs
 remote_chain = RemoteRunnable("http://localhost:8000/chain/")
 print(remote_chain.invoke({"language": "Spanish", "text": "Hi!"}))
